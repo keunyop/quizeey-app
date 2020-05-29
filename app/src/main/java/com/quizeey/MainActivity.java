23906340ea99d4ce88f49ff7209f04d5dc4406d0
keunyop <kylee1112@hotmail.com>
2020-05-29 17:55:02 +0900

    developing

--- app/src/main/java/com/quizeey/MainActivity.java
+++ app/src/main/java/com/quizeey/MainActivity.java
@@ -23,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
 //        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                String javaScript = "javascript:(function() { document.getElementById('footer-placeholder').remove();})()";
-                mWebView.loadUrl(javaScript);
-            }
-        });
+//        mWebView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                String javaScript = "javascript:(function() { document.getElementById('adsense-place').remove();})()";
+//                mWebView.loadUrl(javaScript);
+//            }
+//        });
 
         mWebSettings = mWebView.getSettings(); //세부 세팅 등록
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
@@ -43,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
         mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                String javaScript = "javascript:(function() { document.getElementById('adsense-place').remove();})()";
+                mWebView.loadUrl(javaScript);
+            }
+        });
 
         mWebView.loadUrl("https://quizeey.com"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
     }
